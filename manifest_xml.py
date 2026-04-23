f2c6b96f258656f5b8ed6c13e66ca1bd32179ed8
Fabián Cañas <fabiancanas@google.com>
2026-04-23 16:15:45 +0000

    manifest_xml: Ignore missing projects in remove-project when overriding
    When using a superproject, repo generates an override manifest file.
    During the second load (when the override manifest is used), local
    manifests are applied again. If a local manifest contains a
    remove-project tag for a project that was in the main manifest, it
    will fail because the project was already removed in the first pass
    and not included in the generated override manifest.
    
    This change skips raising ManifestParseError for non-existent projects
    in remove-project if we are loading an override manifest.
    
    Change-Id: I4389485b097fbde552404f2ff2af4ee8415cf974

--- manifest_xml.py
+++ manifest_xml.py
@@ -1644,10 +1644,16 @@ https://gerrit.googlesource.com/git-repo/+/HEAD/docs/manifest-format.md
                     repo_hooks_project = None
 
                 if not removed_project and not XmlBool(node, "optional", False):
-                    raise ManifestParseError(
-                        "remove-project element specifies non-existent "
-                        "project: %s" % node.toxml()
+                    override = self._outer_client.manifestFileOverrides.get(
+                        self.path_prefix
                     )
+                    if not (override and self.manifestFile == override):
+                        print(
+                            "warning: manifest: remove-project element "
+                            "specifies non-existent project: %s"
+                            % node.toxml(),
+                            file=sys.stderr,
+                        )
 
         if failed_revision_changes:
             raise ManifestParseError(
