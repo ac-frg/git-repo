f8fbc29009d968dd86b96185936112c8901c31c1
Gavin Mak <gavinmak@google.com>
2026-05-11 15:56:08 -0700

    sync: Re-raise KeyboardInterrupt in main process
    When running sync -j1, worker functions run directly in the main
    process. Swallowing KeyboardInterrupt causes the loop to continue to the
    next project instead of aborting.
    
    Re-raise KeyboardInterrupt if running in the MainProcess, while
    maintaining the suppression of stack traces in worker processes.
    
    Bug: 468170157
    Change-Id: I156d66bc209a265f7fa25eea0eb88737d1b51a34

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -841,6 +841,8 @@ later is required to fix a server side protocol bug.
                 )
         except KeyboardInterrupt:
             logger.error("Keyboard interrupt while processing %s", project.name)
+            if multiprocessing.current_process().name == "MainProcess":
+                raise
         except GitError as e:
             logger.error("error.GitError: Cannot fetch %s", e)
             errors.append(e)
@@ -1104,6 +1106,8 @@ later is required to fix a server side protocol bug.
             errors.extend(syncbuf.errors)
         except KeyboardInterrupt:
             logger.error("Keyboard interrupt while processing %s", project.name)
+            if multiprocessing.current_process().name == "MainProcess":
+                raise
         except GitError as e:
             logger.error(
                 "error.GitError: Cannot checkout %s: %s", project.name, e
@@ -2406,6 +2410,8 @@ later is required to fix a server side protocol bug.
                 logger.error(
                     "Keyboard interrupt while processing %s", project.name
                 )
+                if multiprocessing.current_process().name == "MainProcess":
+                    raise
             except GitError as e:
                 fetch_errors.append(e)
                 logger.error("error.GitError: Cannot fetch %s", e)
@@ -2456,6 +2462,8 @@ later is required to fix a server side protocol bug.
                     logger.error(
                         "Keyboard interrupt while processing %s", project.name
                     )
+                    if multiprocessing.current_process().name == "MainProcess":
+                        raise
                 except GitError as e:
                     checkout_errors.append(e)
                     logger.error(
